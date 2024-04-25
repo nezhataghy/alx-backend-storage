d94e8ca5bcbb3d9d5f5ff380a9883d2a667f8df1
nezhataghy <nezhataghy@gmail.com>
2024-04-25 21:38:38 +0000

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- lists all bands with Glam rock as their main style, ranked by their longevity
+
+
+SELECT band_name, 
+CASE 
+    WHEN split IS NULL 
+    THEN 2022 - formed
+    ELSE split - formed
+END AS lifespan
+FROM metal_bands
+WHERE STYLE like '%Glam rock%'
+ORDER BY lifespan DESC;
